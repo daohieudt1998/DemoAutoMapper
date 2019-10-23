b34849f5fd99054001090c2b134e5048659a3a8d
Hieu Dao <hieu0987137597@gmail.com>
2019-10-23 16:58:15 +0700

    Add service

---
+++ Service/Interface/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IStudent
+    {
+        IEnumerable<Student> GetAllStudent();
+        Task<Student> GetByIdAsync(int id);
+        Task<Student> CreateAsync(Student student);
+        Task<Student> UpdateAsync(Student student);
+        Task<IActionResult> DeleteAsync(int id);
+    }
+}
---
+++ Service/Interface/IStudentClass.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IStudentClass
+    {
+        IEnumerable<ClassStudent> GetAllStudent();
+        Task<ClassStudent> GetByIdAsync(int id);
+        Task<ClassStudent> CreateAsync(ClassStudent student);
+        Task<ClassStudent> UpdateAsync(ClassStudent student);
+        Task<IActionResult> DeleteAsync(int id);
+    }
+}
